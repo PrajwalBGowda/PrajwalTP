d88d0468b1e26a80d7909b6ddfa547cf25315842
PrajwalBGowda <prajwal.gowda@qualitestgroup.com>
2022-09-07 17:13:22 +0530

    Second Commit

--- src/java/Login.java
+++ src/java/Login.java
@@ -5,6 +5,10 @@ public class Login {
 	public static void main(String[] args) {
 		System.out.println("Enter User Name");
 		System.out.println("Enter Password");
+		System.out.println("Enter User Name");
+		System.out.println("Enter Password");
+		System.out.println("Enter User Name");
+		System.out.println("Enter Password");
 
 	}
 
