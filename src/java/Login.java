ed90f06c83b224d575961707b29c4f668d1855f3
PrajwalBGowda <prajwal.gowda@qualitestgroup.com>
2022-09-07 18:12:03 +0530

    Final Commit

--- src/java/Login.java
+++ src/java/Login.java
@@ -4,25 +4,7 @@ public class Login {
 
 	public static void main(String[] args) {
 		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
-		System.out.println("Enter User Name");
-		System.out.println("Enter Password");
+	
 
 	}
 
